2bb230fc31eb51b74d5a4a8d10d6a0bde3d5533f
Aryan Deora <adeora@iu.edu>
2024-05-19 22:43:15 -0400

    Adding initial spline

---
+++ src/components/react/Banner.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Spline from "@splinetool/react-spline";
+
+export const Banner = () => {
+  return (
+    <div className="flex justify-center pt-12 pb-10 min-h-[464px]">
+      <Spline
+        className="!w-auto"
+        scene="/spline/portfolio_banner_light.spline"
+      />
+    </div>
+  );
+};
---
+++ tailwind.config.mjs
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        light: {
+          50: "#fffefa",
+          100: "#fff7d5",
+          200: "#feecaa",
+          300: "#fddb74",
+          400: "#fbc13c",
+          500: "#f9aa16",
+          600: "#ea8e0c",
+          700: "#c26d0c",
+          800: "#9a5512",
+          900: "#7c4712",
+          950: "#432307",
+        },
+      },
+      screens: {
+        xs: "480px",
+      },
+    },
+  },
+  plugins: [],
+};
